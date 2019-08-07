ccaf4e04dfef5901e416a8775f0c2baf6ae9f675
abinaya1206 <53601556+abinaya1206@users.noreply.github.com>
2019-08-07 13:51:48 +0530

    Create pythonversion.py

---
+++ pythonversion.py
@@ -0,0 +1,13 @@
+import sys
+print("python version")
+print(sys.version)
+print("version_info")
+print(sys.version_info)import sys
+2
+print("python version")
+3
+print(sys version)
+4
+print("version.information")
+5
+print(sys.version_info)
